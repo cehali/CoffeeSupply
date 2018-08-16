d55047ebf840c952e3ba8714284cf5039f1d8d9c
Cezary <cezary.haliniarz@gmail.com>
2018-08-16 16:03:02 +0200

    Changed functionality of providers and added offers layout

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, session, request
+from flask import Flask, render_template, redirect, url_for, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
 from google.cloud import firestore
 
@@ -12,7 +12,6 @@ app.config['SECRET_KEY'] = 'mojbardzosekretnyklucz'
 Bootstrap(app)
 db = firestore.Client()
 users_database = db.collection('users')
-providers_database = db.collection('providers')
 
 
 class LoginForm(FlaskForm):
@@ -24,10 +23,17 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    retypePassword = PasswordField('Retype password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 
 class AddProviderForm(FlaskForm):
-    providerName = SelectField('Select provider', validators=[InputRequired()])
+    # providerName = SelectField('Select provider', validators=[InputRequired()])
+    providerName = StringField('Provider name', validators=[InputRequired()])
+    nrCoffeeBrands = SelectField('Number of coffee brands', coerce=int, validators=[InputRequired()],
+                                 choices=[(x, x) for x in range(1, 100)])
+    frequency = SelectField('Frequency of delivery (for month)', coerce=int, validators=[InputRequired()],
+                            choices=[(x, x) for x in range(1, 31)])
 
 
 @app.route('/')
@@ -41,10 +47,15 @@ def signup():
 
     if not session.get('logged_in'):
         if form.validate_on_submit():
-            hashed_password = generate_password_hash(form.password.data, method='sha256')
-            user = {'username': form.username.data, 'email': form.email.data, 'password': hashed_password, 'providers': []}
-            users_database.add(user)
-            return redirect(url_for('login'))
+            if form.password.data != form.retypePassword.data:
+                form.password.errors.append('Passwords does not match')
+                form.retypePassword.errors.append('Passwords does not match')
+            else:
+                hashed_password = generate_password_hash(form.password.data, method='sha256')
+                user = {'username': form.username.data, 'email': form.email.data, 'password': hashed_password,
+                        'providers': []}
+                users_database.add(user)
+                return redirect(url_for('login'))
     else:
         return redirect(url_for('dashboard'))
 
@@ -64,6 +75,8 @@ def login():
                     session['username'] = form.username.data
                     session['user_id'] = user.id
                     return redirect(url_for('dashboard'))
+            form.username.errors.append('Invalid username or password')
+            form.password.errors.append('Invalid username or password')
     else:
         return redirect(url_for('dashboard'))
 
@@ -84,35 +97,30 @@ def providers():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
 
-    if request.method == 'POST':
+    if form.validate_on_submit():
         users_providers = users_database.document(session['user_id']).get().to_dict().get('providers')
-        if isinstance(users_providers, list):
-            if form.providerName.data not in users_providers:
-                users_providers.append(form.providerName.data)
-                users_database.document(session['user_id']).update({'providers': users_providers})
-        else:
-            if users_providers != form.providerName.data:
-                users_providers = [users_providers]
-                users_providers.append(form.providerName.data)
-                users_database.document(session['user_id']).update({'providers': users_providers})
+        provider = {'name': form.providerName.data, 'numberBrands': form.nrCoffeeBrands.data,
+                    'frequency': form.frequency.data}
+        if provider not in users_providers:
+            users_providers.append(provider)
+            users_database.document(session['user_id']).update({'providers': users_providers})
 
-    providers_ref = providers_database.get()
-    providers = []
-    for provider in providers_ref:
-        providers.append(provider.to_dict().get('name'))
-
-    form.providerName.choices = [(g, g) for g in providers]
+            return redirect(url_for('providers'))
 
     users_providers = users_database.document(session['user_id']).get().to_dict().get('providers')
 
     return render_template('providers.html', form=form, users_providers=users_providers)
 
 
+
 @app.route('/offers')
 def offers():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('offers.html')
+
+    users_providers = users_database.document(session['user_id']).get().to_dict().get('providers')
+
+    return render_template('offers.html', users_providers=users_providers)
 
 
 @app.route('/logout')
